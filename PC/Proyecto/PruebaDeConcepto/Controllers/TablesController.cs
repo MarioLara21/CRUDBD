9f022749f7b1a0e0ae67a3ff6313548214dd16a7
MarioLara21 <mariolara.21@estudianTEC.cr>
2022-09-15 23:43:00 -0600

    Migracion del proyecto anterior
    Migramos el proyecto anterior al git nuevo

---
+++ PC/Proyecto/PruebaDeConcepto/Controllers/TablesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using PruebaDeConcepto.Models;
+namespace PruebaDeConcepto.Controllers
+{
+    public class TablesController : Controller
+    {
+
+        // GET: Tables
+        public ActionResult Index()
+        {
+            List<Usuario> lst;
+            using (BD_01Entities1 db = new BD_01Entities1())
+            {
+                 lst = (from d in db.Usuarios
+                          select new Usuario
+                          {
+                            Id = d.Id,
+                            Nombre = d.Nombre,
+                            Clave = d.Clave,
+                          }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
